e4b15e5c96ca29f988491c256696d72e89060888
Halex43 <heero_43@hotmail.com>
2023-08-16 21:50:35 -0400

    Add project files.

---
+++ Aijoo/Data/Migrations/20230815034028_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aijoo.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Appointment",
+                columns: table => new
+                {
+                    AppointmentID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PetID = table.Column<int>(type: "int", nullable: false),
+                    CustomerID = table.Column<int>(type: "int", nullable: false),
+                    AppointmentDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AppointmentType = table.Column<int>(type: "int", nullable: false),
+                    Notes = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Appointment", x => x.AppointmentID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Appointment");
+        }
+    }
+}
---
+++ Aijoo/Models/Appointment.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Aijoo.Models
+{
+    public class Appointment
+    {
+        public int AppointmentID { get; set; }
+
+        [DisplayName("Pet Profile")]
+        public int PetID { get; set; }
+
+        [DisplayName("Customer Profile")]
+        public int CustomerID { get; set; }
+
+        [Required]
+        [DisplayName("Appointment Request Date")]
+        [DataType(DataType.DateTime)]
+        public DateTime AppointmentDate { get; set; }
+
+        [DisplayName("Appointment Type")]
+        public int AppointmentType { get; set; }
+        public string? Notes { get; set; }
+        public string? Status { get; set; }
+        public Appointment()
+        {
+
+        }
+    }
+}
